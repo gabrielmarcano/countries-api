90e8b9a5548871b5ca714f7057d40d69430b2403
Gabriel Marcano <gabrielmarcano120@gmail.com>
2025-08-17 22:33:51 -0400

    Filter button dropdown

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import {
+  faMagnifyingGlass,
+  faChevronDown,
+} from '@fortawesome/free-solid-svg-icons'
 import CountryCard from '../components/CountryCard'
 import { useEffect, useState } from 'react'
 
 function HomePage() {
   const [inputValue, setInputValue] = useState('')
   const [search, setSearch] = useState('')
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedRegion, setSelectedRegion] = useState('All')
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -24,7 +30,7 @@ function HomePage() {
   return (
     <>
       <div className="flex flex-col items-center p-6">
-        <div className="w-full">
+        <div className="mb-10 w-full">
           <div className="mb-12 flex w-full">
             <div className="flex w-full items-center rounded-md px-8 py-4 shadow-[0_0px_20px_rgba(0,0,0,0.1)]">
               <FontAwesomeIcon
@@ -36,15 +42,47 @@ function HomePage() {
                 onChange={(e) => setInputValue(e.target.value)}
                 value={inputValue}
                 placeholder="Search for a country..."
-                className="ml-6 w-full text-xs text-gray-400 outline-none"
+                className="ml-6 w-full text-sm text-gray-400 outline-none"
               />
             </div>
           </div>
-          <div className="flex w-full justify-start">
-            <button>Filter</button>
-          </div>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="relative flex w-50 cursor-pointer items-center justify-between rounded-md p-4 text-sm shadow-[0_0px_20px_rgba(0,0,0,0.1)]"
+          >
+            <p className="pl-4">
+              {selectedRegion === 'All' ? 'Filter by Region' : selectedRegion}
+            </p>
+            <FontAwesomeIcon icon={faChevronDown} className="text-xs" />
+            <div
+              className={` ${isOpen ? 'flex' : 'hidden'} absolute bottom-0 left-0 w-full translate-y-51/50 cursor-pointer flex-col rounded-md bg-white text-sm shadow-[0_0px_20px_rgba(0,0,0,0.1)]`}
+            >
+              {['All', 'Africa', 'America', 'Asia', 'Europe', 'Oceania'].map(
+                (region) => (
+                  <RegionOption
+                    key={region}
+                    region={region}
+                    onClick={() => {
+                      setSelectedRegion(region)
+                      setIsOpen(false)
+                    }}
+                    top={region === 'All'}
+                    bottom={region === 'Oceania'}
+                  />
+                )
+              )}
+            </div>
+          </button>
         </div>
         <div className="flex w-full flex-col items-center justify-center">
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
+          <CountryCard />
           <CountryCard />
           <CountryCard />
           <CountryCard />
@@ -52,6 +90,27 @@ function HomePage() {
         </div>
       </div>
     </>
+  )
+}
+
+function RegionOption({
+  region,
+  onClick,
+  top,
+  bottom,
+}: {
+  region: string
+  onClick: () => void
+  top?: boolean
+  bottom?: boolean
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className={`w-full cursor-pointer py-2 pl-6 text-left hover:bg-gray-50 ${top && 'rounded-t-md pt-4'} ${bottom && 'rounded-b-md pb-4'}`}
+    >
+      {region}
+    </button>
   )
 }
 
